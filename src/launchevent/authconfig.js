cc3eedc3e13a9b3afccef5268a28a01f4407a960
sushma-sync <sushma@syncsignature.com>
2025-03-05 16:08:52 +0530

    addin

--- src/launchevent/authconfig.js
+++ src/launchevent/authconfig.js
@@ -1,4 +1,4 @@
 export const auth = {
-  clientId: "Enter_the_Application_Id_Here",
+  clientId: "3e201f82-64a7-469e-90d6-28722990edb5",
   authority: "https://login.microsoftonline.com/common",
 };
